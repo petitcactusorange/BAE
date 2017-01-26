25484b48adf3ba2ac4ddeed4dc714733055c7d78
Yasmine Sara Amhis <yasmine.sara.amhis@cern.ch>
2017-01-26 15:13:39 +0100

    ganga for new env

--- ganga_scripts/ganga-bae.py
+++ ganga_scripts/ganga-bae.py
@@ -2,36 +2,31 @@
 
 
 
-taskname = ['MC bae']
+taskname = 'MC bae'
 
 #bu2jpsiK
 #data = ['/MC/2012/Beam4000GeV-2012-MagUp-Nu2.5-Pythia8/Sim08e/Digi13/Trig0x409f0045/Reco14a/Stripping20NoPrescalingFlagged/12143001/ALLSTREAMS.DST']
-
 #SL
 #data = ['/MC/2012/Beam4000GeV-2012-MagUp-Nu2.5-Pythia8/Sim08i/Digi13/Trig0x409f0045/Reco14c/Stripping21r0p1Filtered/10010037/B2KMU.TRIGSTRIP.DST']
 
 #K1mumu
-data = ['/MC/2012/Beam4000GeV-2012-MagDown-Nu2.5-Pythia8/Sim08a/Digi13/Trig0x409f0045/Reco14a/Stripping20NoPrescalingFlagged/12215002/ALLSTREAMS.DST']
+data = '/MC/2012/Beam4000GeV-2012-MagDown-Nu2.5-Pythia8/Sim08a/Digi13/Trig0x409f0045/Reco14a/Stripping20NoPrescalingFlagged/12215002/ALLSTREAMS.DST'
 #data = ['/MC/2012/Beam4000GeV-2012-MagDown-Nu2.5-Pythia8/Sim08d/Digi13/Trig0x409f0045/Reco14a/Stripping20NoPrescalingFlagged/11102202/ALLSTREAMS.DST']
 
 
-
-num=0
-
-ds=BKQuery(path = data[num],dqflag = "All")
+myApplication = GaudiExec()
+myApplication.directory = '/afs/cern.ch/user/y/yamhis/cmtuser/DaVinciDev'
+myApplication.options = ['/afs/cern.ch/user/y/yamhis/BAEO/BAE/davinci_scripts/bae-B2KLLXInclusive_MC.py']
+ds= BKQuery(path = data, dqflag = "All")
 print ds.getDataset()
+
+
 j = Job(
-		name = taskname[num],
+		name = taskname,
 		backend = Dirac(),
-	        application = DaVinci(version =  'v41r0',optsfile = '/afs/cern.ch/user/y/yamhis/BAE/davinci_scripts/bae-B2XMuMu.py'),  
-         	splitter = SplitByFiles(
-			filesPerJob = 20,
-			ignoremissing = True
-			),
-                maxFiles = -1,
-		outputfiles = [
-                LocalFile('*.root'),
-		],
+                application = myApplication,
+         	splitter = SplitByFiles( filesPerJob = 20,ignoremissing = True, maxFiles = -1 ),
+		outputfiles = [ LocalFile('*.root')],
 		inputdata = ds.getDataset()
 		)
 
